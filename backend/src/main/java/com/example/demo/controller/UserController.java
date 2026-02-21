94ce2f042476d6c77b840d95eb5dae89cd466c8e
uri <uri@uri.cz>
2026-02-21 21:42:00 +0100

    pfff i do not even know what im doing rn

---
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,16 @@
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
+public class UserController {
+
+    private final UserRepository repository;
+
+    public UserController(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return repository.findAll();
+    }
+}
---
+++ backend/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,13 @@
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
