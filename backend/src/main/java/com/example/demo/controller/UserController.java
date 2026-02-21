203d97d471669400d3da65f09495a0d8f8cef66c
uri <uri@uri.cz>
2026-02-21 22:14:03 +0100

    importy

--- backend/src/main/java/com/example/demo/controller/UserController.java
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,3 +1,12 @@
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
 @RestController
 @RequestMapping("/api/users")
 @CrossOrigin
--- backend/src/main/java/com/example/demo/entity/User.java
+++ backend/src/main/java/com/example/demo/entity/User.java
@@ -1,3 +1,12 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @Table(name = "users")
 @Getter
